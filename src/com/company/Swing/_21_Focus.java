62f0e2e03caae44cf6053b329429bb269251027a
Nikolay <docnikbolt@gmail.com>
2018-03-28 17:15:08 +0400

    dialogs added

--- src/com/company/Swing/_21_Focus.java
+++ src/com/company/Swing/_21_Focus.java
@@ -1,7 +1,24 @@
 package com.company.Swing;
 
+import com.company.Swing.wrk.myFrame;
+
+import javax.swing.*;
+
 /**
  * Created by Nikolay on 28.03.18.
  */
 public class _21_Focus {
+    public static void main(String[] args) {
+        JFrame jFrame = new myFrame(150,350);
+        final JPanel jPanel = new JPanel();
+        jFrame.add(jPanel);
+
+
+        jPanel.add(new JButton("submit1"));
+        jPanel.add(new JButton("submit2")).setFocusable(false);     // никогда не бывает в фокусе при ТАБуляции
+        jPanel.add(new JButton("submit3")).requestFocusInWindow();  // в фокусе по умолчанию
+
+
+        jPanel.revalidate();
+    }
 }
--- src/com/company/Swing/_22_Dialogs.java
+++ src/com/company/Swing/_22_Dialogs.java
@@ -1,7 +1,31 @@
 package com.company.Swing;
 
+import com.company.Swing.wrk.myFrame;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by Nikolay on 28.03.18.
  */
 public class _22_Dialogs {
+    public static void main(String[] args) {
+        JFrame jFrame = new myFrame(600,800);
+        final JPanel jPanel = new JPanel();
+        jFrame.add(jPanel);
+
+
+        JButton jButton = new JButton("show dialog");
+        jPanel.add(jButton);
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(jPanel, "this is my dialog");
+            }
+        });
+
+
+        jPanel.revalidate();
+    }
 }
